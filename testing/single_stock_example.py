ba1a5397f57f6393a5b7aba80b84b8f49e911553
MFJ <mj@mjoergensen.dk>
2017-10-31 02:21:01 +0100

    Demo of many relevant plot functions now work...

--- testing/single_stock_example.py
+++ testing/single_stock_example.py
@@ -125,6 +125,142 @@ def show_perf_stats(returns, factor_returns, positions=None,
     pf.utils.print_table(perf_stats, fmt='{0:.2f}')
 
 
+
+def prepareMJplots(returns, positions=None,
+                   transactions=None,
+                   live_start_date=None,
+                   cone_std=(1.0, 1.5, 2.0),
+                   benchmark_rets=None,
+                   bootstrap=False,
+                   return_fig=False):
+
+
+    if benchmark_rets is None:
+        benchmark_rets = pf.utils.get_symbol_rets('SPY')
+
+    returns = returns[returns.index > benchmark_rets.index[0]]
+
+    print("Entire data start date: %s" % returns.index[0].strftime('%Y-%m-%d'))
+    print("Entire data end date: %s" % returns.index[-1].strftime('%Y-%m-%d'))
+
+    show_perf_stats(returns, benchmark_rets,
+                             positions=positions,
+                             transactions=transactions,
+                             bootstrap=bootstrap,
+                             live_start_date=live_start_date)
+
+    print("PLEASE WAIT NOW - BE PATIENT - THIS CAN TAKE A WHILE!!!")
+    pf.plotting.show_worst_drawdown_periods(returns)
+
+    # If the strategy's history is longer than the benchmark's, limit strategy
+    if returns.index[0] < benchmark_rets.index[0]:
+        returns = returns[returns.index > benchmark_rets.index[0]]
+
+
+    if live_start_date is not None:
+        live_start_date = ep.utils.get_utc_timestamp(live_start_date)
+
+
+    #fig = plt.figure(figsize=(14, vertical_sections * 6))
+    fig = plt.figure#(figsize=(14, vertical_sections * 6))
+    #gs = gridspec.GridSpec(vertical_sections, 3, wspace=0.5, hspace=0.5)
+    #ax_rolling_returns = plt.subplot(gs[:2, :])
+
+    ax_returns = pf.plotting.plot_returns(
+        returns,
+        live_start_date=live_start_date)
+    ax_returns.set_title(
+        'Returns')
+    plt.show()
+
+    ax_rolling_returns = pf.plotting.plot_rolling_returns(
+        returns,
+        factor_returns=benchmark_rets,
+        live_start_date=live_start_date,
+        cone_std=cone_std)
+    ax_rolling_returns.set_title(
+        'Cumulative returns')
+    plt.show()
+
+    ax_rolling_returns_vol_match = pf.plotting.plot_rolling_returns(
+        returns,
+        factor_returns=benchmark_rets,
+        live_start_date=live_start_date,
+        cone_std=None,
+        volatility_match=True,
+        legend_loc=None)
+    ax_rolling_returns_vol_match.set_title(
+        'Cumulative returns volatility matched to benchmark')
+    plt.show()
+
+    ax_rolling_returns_log = pf.plotting.plot_rolling_returns(
+        returns,
+        factor_returns=benchmark_rets,
+        logy=True,
+        live_start_date=live_start_date,
+        cone_std=cone_std)
+    ax_rolling_returns_log.set_title(
+        'Cumulative returns on logarithmic scale')
+    plt.show()
+
+    print("Plotting beta can be slow!")
+    ax_rolling_beta = pf.plotting.plot_rolling_beta(
+        returns, benchmark_rets)
+    plt.show()
+
+    ax_rolling_volatility = pf.plotting.plot_rolling_volatility(
+        returns, factor_returns=benchmark_rets)
+    plt.show()
+
+    ax_rolling_sharpe = pf.plotting.plot_rolling_sharpe(
+        returns)
+    plt.show()
+
+    print("Fama french can be slow!")
+    ax_rolling_risk = pf.plotting.plot_rolling_fama_french(
+        returns)
+    plt.show()
+
+    # Drawdowns
+    ax_drawdown = pf.plotting.plot_drawdown_periods(
+        returns, top=5)
+    plt.show()
+
+    ax_underwater = pf.plotting.plot_drawdown_underwater(
+        returns=returns)
+    plt.show()
+
+    ax_monthly_heatmap = pf.plotting.plot_monthly_returns_heatmap(returns)
+    plt.show()
+
+    ax_annual_returns = pf.plotting.plot_annual_returns(returns)
+    plt.show()
+
+    ax_monthly_dist = pf.plotting.plot_monthly_returns_dist(returns)
+    plt.show()
+
+    print("Plotting return quantiles can be slow...")
+    ax_return_quantiles = pf.plotting.plot_return_quantiles(
+        returns,
+        live_start_date=live_start_date)
+    plt.show()
+
+    if bootstrap:
+        print("This can be VERY slow...")
+        ax_bootstrap = pf.plotting.plot_perf_stats(returns, benchmark_rets)
+        plt.show()
+
+    #for ax in fig.axes:
+    #    plt.setp(ax.get_xticklabels(), visible=True)
+
+    #plt.show()
+    if return_fig:
+        return fig
+
+
+
+
+
 def create_returns_tear_sheet(returns, positions=None,
                               transactions=None,
                               live_start_date=None,
@@ -171,9 +307,6 @@ def create_returns_tear_sheet(returns, positions=None,
         If True, set default plotting style context.
     """
 
-    #myDebug = True # THIS DOES NOT WORK!
-    myDebug = False # THIS DOES NOT WORK!
-
     if benchmark_rets is None:
         benchmark_rets = pf.utils.get_symbol_rets('SPY')
 
@@ -188,14 +321,8 @@ def create_returns_tear_sheet(returns, positions=None,
                              bootstrap=bootstrap,
                              live_start_date=live_start_date)
 
-    if myDebug:
-        plt.show()
-
     print("PLEASE WAIT NOW - BE PATIENT - THIS CAN TAKE A WHILE!!!")
     pf.plotting.show_worst_drawdown_periods(returns)
-
-    if myDebug:
-        plt.show()
 
     # If the strategy's history is longer than the benchmark's, limit strategy
     if returns.index[0] < benchmark_rets.index[0]:
@@ -223,6 +350,7 @@ def create_returns_tear_sheet(returns, positions=None,
     i += 1
     ax_returns = plt.subplot(gs[i, :],
                              sharex=ax_rolling_returns)
+
     i += 1
     ax_rolling_beta = plt.subplot(gs[i, :], sharex=ax_rolling_returns)
     i += 1
@@ -241,6 +369,7 @@ def create_returns_tear_sheet(returns, positions=None,
     ax_monthly_dist = plt.subplot(gs[i, 2])
     i += 1
     ax_return_quantiles = plt.subplot(gs[i, :])
+
     i += 1
 
     pf.plotting.plot_rolling_returns(
@@ -249,6 +378,7 @@ def create_returns_tear_sheet(returns, positions=None,
         live_start_date=live_start_date,
         cone_std=cone_std,
         ax=ax_rolling_returns)
+
     ax_rolling_returns.set_title(
         'Cumulative returns')
 
@@ -281,61 +411,33 @@ def create_returns_tear_sheet(returns, positions=None,
     ax_returns.set_title(
         'Returns')
 
-    if myDebug:
-        plt.show()
-
-
     pf.plotting.plot_rolling_beta(
         returns, benchmark_rets, ax=ax_rolling_beta)
-
-    if myDebug:
-        plt.show()
 
     pf.plotting.plot_rolling_volatility(
         returns, factor_returns=benchmark_rets, ax=ax_rolling_volatility)
 
-    if myDebug:
-        plt.show()
-
     pf.plotting.plot_rolling_sharpe(
         returns, ax=ax_rolling_sharpe)
 
-    if myDebug:
-        plt.show()
-
     pf.plotting.plot_rolling_fama_french(
         returns, ax=ax_rolling_risk)
-
-    if myDebug:
-        plt.show()
 
     # Drawdowns
     pf.plotting.plot_drawdown_periods(
         returns, top=5, ax=ax_drawdown)
 
-    if myDebug:
-        plt.show()
-
     pf.plotting.plot_drawdown_underwater(
         returns=returns, ax=ax_underwater)
-
-    if myDebug:
-        plt.show()
 
     pf.plotting.plot_monthly_returns_heatmap(returns, ax=ax_monthly_heatmap)
     pf.plotting.plot_annual_returns(returns, ax=ax_annual_returns)
     pf.plotting.plot_monthly_returns_dist(returns, ax=ax_monthly_dist)
 
-    if myDebug:
-        plt.show()
-
     pf.plotting.plot_return_quantiles(
         returns,
         live_start_date=live_start_date,
         ax=ax_return_quantiles)
-
-    if myDebug:
-        plt.show()
 
     if bootstrap:
         ax_bootstrap = plt.subplot(gs[i, :])
@@ -370,6 +472,11 @@ stock_rets = pf.utils.get_symbol_rets('FB')
 # ## Create a returns tear sheet for the single stock
 # This will show charts and analysis about returns of the single stock.
 print("Please wait: pf.create_returns_tear_sheet(stock_rets, live_start_date='2015-12-1') - WAIT PLEASE!")
-create_returns_tear_sheet(stock_rets, live_start_date='2015-12-1')
+
+if False:
+    create_returns_tear_sheet(stock_rets, live_start_date='2015-12-1')
+else:
+    prepareMJplots(stock_rets, live_start_date='2015-12-1')
+
 
 print("done")
